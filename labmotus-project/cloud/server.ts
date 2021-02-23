4766d1ab363f8d7919cc4316400ec5be898862e5
Sayan Faraz <sayan96@hotmail.com>
2021-02-23 12:05:09 -0500

    Boilerplate code

--- labmotus-project/cloud/server.ts
+++ labmotus-project/cloud/server.ts
@@ -1,20 +1,26 @@
-const fastify = require('fastify')({
-    logger: true
-  })
-  
-  fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
-  })
-  
-  const start = async () => {
-    try {
-      await fastify.listen(3000)
-    } catch (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
+import * as fastify from 'fastify'
+import { Server, IncomingMessage, ServerResponse } from 'http'
+
+// Create a http server. We pass the relevant typings for our http version used.
+// By passing types we get correctly typed access to the underlying http objects in routes.
+// If using http2 we'd pass <http2.Http2Server, http2.Http2ServerRequest, http2.Http2ServerResponse>
+const server: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify.fastify()
+
+server.get('/', async (request, reply) => {
+  console.log(reply.raw) // this is the http.ServerResponse with correct typings!
+  // reply.code(200).send("")
+  return "Hello World"
+})
+
+server.listen(8080, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
   }
-  start()
+  console.log(`Server listening at ${address}`)
+})
+
+
 
 //Server Features
 // /user API calls (all use auth token passed through Authorization header) 
