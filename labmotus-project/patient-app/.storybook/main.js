8a81b4961bc0a207d580b48d2ad039bb04374517
Yi Cheng Zhu <yicheng.zhu@mail.utoronto.ca>
2021-03-01 10:14:23 -0600

    Fixed NPM workspaces import issues. Yay!

--- labmotus-project/patient-app/.storybook/main.js
+++ labmotus-project/patient-app/.storybook/main.js
@@ -1,3 +1,6 @@
+const {removeModuleScopePlugin} = require('customize-cra');
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -7,5 +10,22 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)?$/,
+      include: path.resolve(__dirname, '../../'),
+      exclude: [path.resolve(__dirname, '../../patient-app'), path.resolve(__dirname, '../../clinician-app')],
+      use: [
+        {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true
+          }
+        }
+      ]
+    });
+    removeModuleScopePlugin()(config);
+    return config;
+  },
 };
