131fef7808904febb9252d97b6e981654f73f0d2
Sayan Faraz <sayan96@hotmail.com>
2021-03-02 23:53:34 -0500

    Arrow

--- labmotus-project/clinician-app/src/components/PatientsListComponent.tsx
+++ labmotus-project/clinician-app/src/components/PatientsListComponent.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { Patient } from "../../../common/types/types"
-import { IonItem, IonLabel } from "@ionic/react"
+import { IonIcon, IonItem, IonLabel } from "@ionic/react"
+import { ProfilePictureComponent } from "@labmotus/ui";
+import { chevronForward } from 'ionicons/icons';
 
 export interface PatientListProp {
   patientList: Array<Patient>
@@ -29,11 +31,12 @@ export const PatientListing: React.FC<Patient> = (patient: Patient) => {
   return(
     <div>
       <IonItem>
+        <ProfilePictureComponent imageLink="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y"/>
           <IonLabel>
             <h1>{patient.user.name}</h1>
             <h3>{patient.phone}</h3>
-            {/* <p>Listen, I've had a pretty messed up day...</p> */}
           </IonLabel>
+          <IonIcon icon={chevronForward} />
       </IonItem>
     </div>
   )
