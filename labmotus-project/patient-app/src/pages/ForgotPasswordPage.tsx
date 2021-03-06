d4bdfb58113da0ad1d6057946ebdec31a1fc32c7
sun-yuhan <yuhan25@hotmail.com>
2021-03-06 16:02:32 -0500

    restyle signup/login/forgot pass pages

--- labmotus-project/patient-app/src/pages/ForgotPasswordPage.tsx
+++ labmotus-project/patient-app/src/pages/ForgotPasswordPage.tsx
@@ -1,5 +1,5 @@
 import React, {FunctionComponent, useContext, useState} from "react";
-import {IonAlert, IonButton, IonInput} from "@ionic/react";
+import {IonAlert, IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonInput, IonPage, IonToolbar} from "@ionic/react";
 // @ts-ignore
 import styled from "styled-components";
 import {APIContext} from "../api/API";
@@ -16,8 +16,8 @@ const ForgotPasswordPage: FunctionComponent<ForgotPasswordPageProps> = () => {
 
     async function forgotPassword() {
         if (!email) {
-            setHeader("Invalid email");
-            setMessage("Please enter your email");
+            setHeader("Invalid Email");
+            setMessage("Please enter your email.");
             openAlert(true);
             return;
         }
@@ -26,70 +26,73 @@ const ForgotPasswordPage: FunctionComponent<ForgotPasswordPageProps> = () => {
         } catch (e) {
             console.error(e)
         }
-        // TODO: USE FIREBASE AND CONNECT TO BACKEND
-        // firebase
-        //   .auth()
-        //   .sendPasswordResetEmail(email)
-        //   .then(() => {
-        //     // Email sent.
-        //   })
-        //   .catch((error) => {
-        //     var errorCode = error.code;
-        //     var errorMessage = error.message;
-        //   });
     }
 
     return (
-        <ForgotPasswordPageDiv>
-            <h1>Reset Password</h1>
-            <p>
-                Enter the email address you used to register and we'll send you the
-                instructions for resetting your password.
-            </p>
-            <IonInput
-        class="input email"
-        placeholder="Email"
-        type="email"
-        inputmode="email"
-        value={email}
-        onIonChange={(e) => setEmail(e.detail.value!)}
-            />
-      <IonButton expand="block" shape="round" onClick={forgotPassword}>
-        Submit
-      </IonButton>
-      <IonAlert
-        isOpen={iserror}
-        onDidDismiss={() => openAlert(false)}
-        header={header}
-        message={message}
-        buttons={["OK"]}
-      />
-    </ForgotPasswordPageDiv>
+        <IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonButtons slot="start">
+						<IonBackButton defaultHref="/login" />
+					</IonButtons>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent fullscreen>
+				<ForgotPasswordPageDiv>
+					<h1>Reset Password</h1>
+					<div className="main">
+						<p>
+							Enter the email address you used to register and we'll send you the
+							instructions for resetting your password.
+						</p>
+						<IonInput
+						class="input email"
+						placeholder="Email"
+						type="email"
+						inputmode="email"
+						value={email}
+						onIonChange={(e) => setEmail(e.detail.value!)}
+						/>
+						<IonButton expand="block" shape="round" onClick={forgotPassword}>
+							Submit
+						</IonButton>
+					</div>
+				</ForgotPasswordPageDiv>
+			</IonContent>
+		<IonAlert
+			isOpen={iserror}
+			onDidDismiss={() => openAlert(false)}
+			header={header}
+			message={message}
+			buttons={["OK"]}
+		/>
+    </IonPage>
   );
 };
 
 const ForgotPasswordPageDiv = styled.div`
-  overflow: hidden;
-  text-align: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 100%;
-  padding: 5%;
-  > * {
-    margin-bottom: 2.5%;
-  }
-  .input {
-    margin-bottom: 5%;
-    text-align: left;
-    border-radius: 5px;
-    border: 1px solid #ddd;
-  }
-  h1 {
-    font-weight: bold;
-    margin: 5vh 0;
-  }
+	overflow: hidden;
+	text-align: center;
+	padding: 5%;
+	.main {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		width: 100%;
+		padding: 5%;
+		.input {
+			margin-bottom: 5%;
+			text-align: left;
+			border-radius: 5px;
+			border: 1px solid #ddd;
+			--padding-start: 10px;
+		}
+	}
+	h1 {
+		font-weight: bold;
+		margin-top: 15vh;
+	}
 `;
 
 export default ForgotPasswordPage;
--- labmotus-project/patient-app/src/pages/SignupPage.tsx
+++ labmotus-project/patient-app/src/pages/SignupPage.tsx
@@ -1,5 +1,5 @@
 import React, {FunctionComponent, useContext, useState} from "react";
-import {IonAlert, IonButton, IonInput} from "@ionic/react";
+import {IonAlert, IonButton, IonContent, IonInput, IonPage} from "@ionic/react";
 // @ts-ignore
 import styled from "styled-components";
 import {APIContext} from "../api/API";
@@ -9,96 +9,92 @@ export interface SignupPageProps {
 }
 
 const SignupPage: FunctionComponent<SignupPageProps> = () => {
-  const API = useContext(APIContext);
-  const history = useHistory();
-  const [email, setEmail] = useState<string>();
-  const [password, setPassword] = useState<string>();
-  const [confirmPassword, setConfirmPassword] = useState<string>();
-  const [iserror, openAlert] = useState<boolean>(false);
-  const [header, setHeader] = useState<string>();
-  const [message, setMessage] = useState<string>();
+	const API = useContext(APIContext);
+	const history = useHistory();
+	const [email, setEmail] = useState<string>();
+	const [password, setPassword] = useState<string>();
+	const [confirmPassword, setConfirmPassword] = useState<string>();
+	const [iserror, openAlert] = useState<boolean>(false);
+	const [header, setHeader] = useState<string>();
+	const [message, setMessage] = useState<string>();
 
-  async function signUp() {
-    if (!email) {
-      setHeader("Invalid email");
-      setMessage("Please enter your email");
-      openAlert(true);
-      return;
-    }
+	async function signUp() {
+		if (!email) {
+			setHeader("Invalid Email");
+			setMessage("Please enter your email.");
+			openAlert(true);
+			return;
+		}
 
-    if (!password) {
-      setHeader("Invalid Password");
-      setMessage("Please enter a password");
-      openAlert(true);
-      return;
-    }
+		if (!password) {
+			setHeader("Invalid Password");
+			setMessage("Please enter a password.");
+			openAlert(true);
+			return;
+		}
 
-    if (password !== confirmPassword) {
-      setHeader("Passwords Don't Match");
-      setMessage("The passwords don't match. Please try again.");
-      openAlert(true);
-      return;
-    }
+		if (password !== confirmPassword) {
+			setHeader("Passwords Don't Match");
+			setMessage("The passwords don't match. Please try again.");
+			openAlert(true);
+			return;
+		}
 
-    try {
-      await API.signUp(email, password);
-      history.push('/home');
-    } catch (e) {
-      console.error(e)
-    }
+		try {
+			await API.signUp(email, password);
+			history.push('/home');
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-    // TODO: USE FIREBASE AND CONNECT TO BACKEND
-    // firebase
-    //   .auth()
-    //   .createUserWithEmailAndPassword(email, password)
-    //   .then((userCredential) => {
-    //     // SIGNED IN
-    //     var user = userCredential.user;
-    //     // TODO: CREATE ACCOUNT AND REDIRECT TO HOME PAGE
-    //   })
-    //   .catch((error) => {
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //   });
-  }
+	function login() {
+		history.push('/login');
+	}
 
   return (
-    <SignupPageDiv>
-      <h1>LabMotus</h1>
-      <form>
-        <IonInput
-          class="input email"
-          placeholder="Email"
-          type="email"
-          inputmode="email"
-          value={email}
-          onIonChange={(e) => setEmail(e.detail.value!)}
-        />
-        <IonInput
-          class="input password"
-          placeholder="Password"
-          type="password"
-          value={password}
-          onIonChange={(e) => setPassword(e.detail.value!)}
-        />
-        <IonInput
-          class="input password"
-          placeholder="Confirm Password"
-          type="password"
-          value={confirmPassword}
-          onIonChange={(e) => setConfirmPassword(e.detail.value!)}
-        />
+    <IonPage>
+		<IonContent fullscreen>
+			<SignupPageDiv>
+				<h1>LabMotus</h1>
+				<div className="main">
+					<form>
+						<IonInput
+						class="input email"
+						placeholder="Email"
+						type="email"
+						inputmode="email"
+						value={email}
+						onIonChange={(e) => setEmail(e.detail.value!)}
+						/>
+						<IonInput
+						class="input password"
+						placeholder="Password"
+						type="password"
+						value={password}
+						onIonChange={(e) => setPassword(e.detail.value!)}
+						/>
+						<IonInput
+						class="input password"
+						placeholder="Confirm Password"
+						type="password"
+						value={confirmPassword}
+						onIonChange={(e) => setConfirmPassword(e.detail.value!)}
+						/>
 
-        <IonButton expand="block" shape="round" onClick={signUp}>
-          Sign Up
-        </IonButton>
-      </form>
-      <p>
-        By clicking 'Sign Up' you agree to our <a>Terms of Service</a>
-      </p>
-      <p id="bottom">
-        Already have an account? <a>Login</a>
-      </p>
+						<IonButton expand="block" shape="round" onClick={signUp}>
+						Sign Up
+						</IonButton>
+					</form>
+					<p>
+						By clicking 'Sign Up' you agree to our <a>Terms of Service</a>
+					</p>
+				</div>
+				<p className="footer">
+					Already have an account? <a onClick={login}>Login</a>
+				</p>
+			</SignupPageDiv>
+	  </IonContent>
       <IonAlert
         isOpen={iserror}
         onDidDismiss={() => openAlert(false)}
@@ -106,41 +102,38 @@ const SignupPage: FunctionComponent<SignupPageProps> = () => {
         message={message}
         buttons={["OK"]}
       />
-    </SignupPageDiv>
+    </IonPage>
   );
 };
 
 const SignupPageDiv = styled.div`
-  overflow: hidden;
-  text-align: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 100%;
-  padding: 5%;
-  > * {
-    margin-bottom: 2.5%;
-  }
-  form {
-      
-  }
-  .input {
-    margin-bottom: 5%;
-    text-align: left;
-    border-radius: 5px;
-    border: 1px solid #ddd;
-  }
-  h1 {
-    font-weight: bold;
-  }
-  h1,
-  #bottom {
-    margin: 10vh 0;
-  }
-  a {
-    text-decoration: none;
-  }
+overflow: hidden;
+text-align: center;
+.main {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		width: 100%;
+		padding: 5%;
+		.input {
+			margin-bottom: 5%;
+			text-align: left;
+			border-radius: 5px;
+			border: 1px solid #ddd;
+			--padding-start: 10px;
+		}
+}
+h1 {
+		font-weight: bold;
+		margin-top: 15vh;
+}
+.footer {
+		margin-top: 65vh;
+}
+a {
+		text-decoration: none;
+}
 `;
 
 export default SignupPage;
