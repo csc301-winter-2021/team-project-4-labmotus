577bd728e3d5a1248c73731c59343d22fc23baa7
David Canagasabey <david@dtcan.dev>
2021-03-24 16:04:00 -0400

    Implement Lambda function to call wrnch API

--- labmotus-project/lambda/index.js
+++ labmotus-project/lambda/index.js
@@ -1,8 +1,131 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+const AWS = require('aws-sdk');
+
+const awsParams = { region: 'us-east-1' };
+const S3 = new AWS.S3(awsParams);
+const DynamoDB = new AWS.DynamoDB.DocumentClient(awsParams);
+
+const KEY_PREFIX = "assessment_";
+const ASSESSMENTS_TABLE = "labmotus-assessments";
+const JOB_ID_COLUMN = "wrnchJob";
+const ASSESSMENTS_STATE_COLUMN = "state";
+const WRNCH_API = "https://api.wrnch.ai/v1/";
+
+function checkAssessment(id) {
+    return new Promise((resolve, reject) => {
+        console.log(`Checking if assessment ${id} exists`);
+        DynamoDB.get({
+            TableName: ASSESSMENTS_TABLE,
+            Key: { id }
+        }).send((err, _) => {
+            if(err) {
+                reject(err);
+            }else {
+                console.log(`Assessment ${id} exists!`);
+                resolve();
+            }
+        });
+    });
+}
+
+function getVideoFile(bucket, key) {
+    return new Promise((resolve, reject) => {
+        console.log(`Retrieving ${key} from ${bucket}`);
+        S3.getObject({
+            Bucket: bucket,
+            Key: key
+        }).send((err, data) => {
+            if(err) {
+                reject(err);
+            }else {
+                console.log(`${key} retrieved!`);
+                resolve(data.Body);
+            }
+        });
+    });
+}
+
+function uploadToWrnch(file, filename) {
+    return new Promise((resolve, reject) => {
+        console.log(`Creating new wrnch job for ${filename}`);
+        fs.readFile("WRNCH_API_KEY", (err, data) => {
+            if(err) {
+                reject(err);
+            }else {
+                /* global URLSearchParams */
+                var loginParams = new URLSearchParams();
+                loginParams.append('api_key', data.toString());
+
+                var jobParams = new FormData();
+                jobParams.append('media', file, { filename });
+                jobParams.append('work_type', 'json');
+                jobParams.append('est_3d', 'true');
+
+                fetch(WRNCH_API + "login", {
+                    method: 'POST',
+                    body: loginParams
+                })
+                .then(r => r.json())
+                .then(res => res["access_token"])
+                .then(token => fetch(WRNCH_API + "jobs", {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': "Bearer " + token
+                    },
+                    body: jobParams
+                }))
+                .then(r => r.json())
+                .then(res => {
+                    console.log(`Job for ${filename} created!`);
+                    resolve(res["job_id"])
+                })
+                .catch(reject);
+            }
+        });
+    });
+}
+
+function updateAssessment(assessmentId, jobId) {
+    return new Promise((resolve, reject) => {
+        console.log(`Updating state for assessment ${assessmentId}`);
+        DynamoDB.update({
+            TableName: ASSESSMENTS_TABLE,
+            Key: {
+                id: assessmentId
+            },
+            UpdateExpression: 'set #J = :j, #S = :s',
+            ExpressionAttributeNames: {
+                '#J': JOB_ID_COLUMN,
+                '#S': ASSESSMENTS_STATE_COLUMN
+            },
+            ExpressionAttributeValues: {
+                ':j': jobId,
+                ':s': "PENDING"
+            }
+        }).send((err, _) => {
+            if(err) {
+                reject(err);
+            }else {
+                console.log(`Assessment ${assessmentId} updated!`);
+                resolve();
+            }
+        });
+    });
+}
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    const srcBucket = event.Records[0].s3.bucket.name;
+    const srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+
+    if(srcKey.startsWith(KEY_PREFIX)) {
+        const assessmentId = srcKey.substring(KEY_PREFIX.length, srcKey.length - 4);
+        checkAssessment(assessmentId).catch(console.error)
+        .then(() => getVideoFile(srcBucket, srcKey)).catch(console.error)
+        .then(video => uploadToWrnch(video, srcKey)).catch(console.error)
+        .then(jobId => updateAssessment(assessmentId, jobId)).catch(console.error);
+    }else {
+        console.log(`${srcKey} is not an assessment`);
+    }
 };
